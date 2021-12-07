606a37606986b4d8c30a7b6f1af6aa269b014a44
jefferygeng <77049892+jefferygeng@users.noreply.github.com>
2021-12-08 00:26:47 +0800

    Add files via upload

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package swag converts Go annotations to Swagger Documentation 2.0.
+See https://github.com/jefferygeng/swag for more information about swag.
+*/
+package swag // import "github.com/jefferygeng/swag"
